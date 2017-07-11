5993a05d35927198fbe179f1ba85f73feeec5332
q9029 <key.ken0215@gmail.com>
2017-07-11 20:42:06 +0900

    develop

--- src/main/java/com/github/q9029/aikatsustars/controller/SearchController.java
+++ src/main/java/com/github/q9029/aikatsustars/controller/SearchController.java
@@ -4,14 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.github.q9029.aikatsustars.controller.constant.RequestKey;
 import com.github.q9029.aikatsustars.controller.constant.RequestURI;
 import com.github.q9029.aikatsustars.controller.constant.View;
+import com.github.q9029.aikatsustars.form.SearchFormBean;
+import com.github.q9029.aikatsustars.service.SearchService;
 
 import twitter4j.TwitterException;
 
@@ -21,8 +25,11 @@ public class SearchController {
 
 	private static final Logger LOG = Logger.getLogger(SearchController.class);
 
+	@Autowired
+	private SearchService searchService;
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String doGet(HttpServletRequest request, HttpSession session) throws TwitterException {
+	public String doGet(HttpServletRequest request, HttpSession session, @ModelAttribute SearchFormBean formBean) throws TwitterException {
 
 		String keyword = request.getParameter(RequestKey.Parameter.KEYWORD);
 		String volume = request.getParameter(RequestKey.Parameter.VOLUME);
