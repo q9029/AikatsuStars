1dad42f7f34daede9251e2638af04861f63b508b
q9029 <key.ken0215@gmail.com>
2017-07-11 20:21:48 +0900

    develop

--- src/main/java/com/github/q9029/aikatsustars/controller/SearchController.java
+++ src/main/java/com/github/q9029/aikatsustars/controller/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.github.q9029.aikatsustars.controller.constant.RequestKey;
 import com.github.q9029.aikatsustars.controller.constant.RequestURI;
 import com.github.q9029.aikatsustars.controller.constant.View;
 
@@ -18,24 +19,35 @@ import twitter4j.TwitterException;
 @RequestMapping(value = RequestURI.SEARCH)
 public class SearchController {
 
-    /**
-     * ロガー.
-     */
-    private static final Logger LOG = Logger.getLogger(SearchController.class);
+	private static final Logger LOG = Logger.getLogger(SearchController.class);
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String doGet(HttpServletRequest request, HttpSession session) throws TwitterException {
-        return View.SEARCH;
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String doGet(HttpServletRequest request, HttpSession session) throws TwitterException {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String doPost(HttpServletRequest request, HttpSession session) throws TwitterException {
-        return "redirect:/search";
-    }
+		String keyword = request.getParameter(RequestKey.Parameter.KEYWORD);
+		String volume = request.getParameter(RequestKey.Parameter.VOLUME);
+		String type = request.getParameter(RequestKey.Parameter.TYPE);
+		String category = request.getParameter(RequestKey.Parameter.CATEGORY);
+		String brand = request.getParameter(RequestKey.Parameter.BRAND);
+		String raretiry = request.getParameter(RequestKey.Parameter.RARITY);
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception e) {
-        LOG.error("System error.", e);
-        return View.SEARCH;
-    }
+		request.setAttribute(RequestKey.Attribute.VOLUME_LIST, null);
+		request.setAttribute(RequestKey.Attribute.TYPE_LIST, null);
+		request.setAttribute(RequestKey.Attribute.CATEGORY_LIST, null);
+		request.setAttribute(RequestKey.Attribute.BRAND_LIST, null);
+		request.setAttribute(RequestKey.Attribute.RARITY_LIST, null);
+
+		return View.SEARCH;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String doPost(HttpServletRequest request, HttpSession session) throws TwitterException {
+		return "redirect:/search";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e) {
+		LOG.error("System error.", e);
+		return View.SEARCH;
+	}
 }
--- src/main/java/com/github/q9029/aikatsustars/controller/constant/RequestKey.java
+++ src/main/java/com/github/q9029/aikatsustars/controller/constant/RequestKey.java
@@ -1,12 +1,27 @@
 package com.github.q9029.aikatsustars.controller.constant;
 
-/**
- * HttpServletRequestで使用するキーの定数クラス.
- */
 public class RequestKey {
 
-    private RequestKey() {}
+	private RequestKey() {}
 
-    public static final String OAUTH_VERIFIER = "oauth_verifier";
-    public static final String TWEET = "tweet";
+	public static final String OAUTH_VERIFIER = "oauth_verifier";
+	public static final String TWEET = "tweet";
+
+
+	public class Attribute {
+		public static final String VOLUME_LIST = "volumeList";
+		public static final String TYPE_LIST = "typeList";
+		public static final String CATEGORY_LIST = "categoryList";
+		public static final String BRAND_LIST = "brandList";
+		public static final String RARITY_LIST = "rarityList";
+	}
+
+	public class Parameter {
+		public static final String KEYWORD = "keyword";
+		public static final String VOLUME = "volume";
+		public static final String TYPE = "type";
+		public static final String CATEGORY = "category";
+		public static final String BRAND = "brand";
+		public static final String RARITY = "rarity";
+	}
 }
