c4c76744d6ac48ad0dc3b4c48f11d6a16bcf9e7e
q9029 <key.ken0215@gmail.com>
2017-07-25 20:43:38 +0900

    develop

--- src/main/java/com/github/q9029/starstrade/controller/CardListController.java
+++ src/main/java/com/github/q9029/starstrade/controller/CardListController.java
@@ -1,6 +1,7 @@
 package com.github.q9029.starstrade.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 
 import com.github.q9029.starstrade.controller.constant.RequestUri;
 import com.github.q9029.starstrade.controller.constant.View;
+import com.github.q9029.starstrade.repository.dto.CardDto;
 import com.github.q9029.starstrade.service.CardsService;
 
 import twitter4j.TwitterException;
@@ -23,10 +26,22 @@ public class CardListController {
 	private CardsService cardsService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String doGet(HttpServletRequest request, HttpSession session) throws TwitterException {
+	public String doGet(WebRequest request, HttpSession session) throws TwitterException {
+
+		String name = request.getParameter("name");
+
+		String category = request.getParameter("category");
+		String brand = request.getParameter("brand");
+		String rareriry = request.getParameter("rareriry");
+
 		String volume = request.getParameter("volume");
-		volume = !StringUtils.isEmpty(volume) ? volume : "1";
-		request.setAttribute("cardlist", cardsService.getAllCards());
+		if (StringUtils.isEmpty(volume)) {
+			volume = "1";
+		}
+
+		List<CardDto> cardList = cardsService.getVolumeCards(Integer.parseInt(volume));
+		request.setAttribute("cardlist", cardList, WebRequest.SCOPE_REQUEST);
+
 		return View.CARD_LIST;
 	}
 }
