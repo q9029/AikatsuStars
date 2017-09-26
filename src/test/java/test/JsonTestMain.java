b1521b32b9eb258f85e7c5cdba099c9f410eb981
q9029 <key.ken0215@gmail.com>
2017-09-26 11:39:22 +0900

    develp

--- src/test/java/test/JsonTestMain.java
+++ src/test/java/test/JsonTestMain.java
@@ -3,33 +3,54 @@ package test;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import net.arnx.jsonic.JSON;
 
 public class JsonTestMain {
 
+	private static final Logger LOG = Logger.getLogger(JsonTestMain.class);
+
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 
-		Map<String, Object> cardInfoMap = new LinkedHashMap<>();
-		cardInfoMap.put("id", 1);
-		cardInfoMap.put("no", "PR 1-1-1");
-		cardInfoMap.put("name", "ドレス名");
-		cardInfoMap.put("type", "クール");
-		cardInfoMap.put("brand", "スパイスコード");
-		cardInfoMap.put("rarerity", "SPR");
-		cardInfoMap.put("volume", "星のツバサ1弾");
-		cardInfoMap.put("img_url", "https://hogehoge.hogehoge.com/img/image.png");
+		int exitCode = 0;
 
-		String json = JSON.encode(cardInfoMap);
-		System.out.println(json);
-		// {
-		//  "id":1,
-		//  "no":"PR 1-1-1",
-		//  "name":"ドレス名",
-		//  "type":"クール",
-		//  "brand":"スパイスコード",
-		//  "rarerity":"SPR",
-		//  "volume":"星のツバサ1弾",
-		//  "img_url":"https://hogehoge.hogehoge.com/img/image.png"
-		// }
+		try {
+			LOG.info("処理を開始します。");
+
+			Map<String, Object> jsonKeyValueMap = new LinkedHashMap<>();
+			jsonKeyValueMap.put("id", 1);
+			jsonKeyValueMap.put("no", "PR 1-1-1");
+			jsonKeyValueMap.put("name", "ドレス名");
+			jsonKeyValueMap.put("type", "クール");
+			jsonKeyValueMap.put("brand", "スパイスコード");
+			jsonKeyValueMap.put("rarerity", "SPR");
+			jsonKeyValueMap.put("volume", "星のツバサ1弾");
+
+			if (LOG.isDebugEnabled()) {
+				for (String key : jsonKeyValueMap.keySet()) {
+					LOG.debug("map:key=" + key + ",value=" + jsonKeyValueMap.get(key));
+				}
+			}
+
+			String json = JSON.encode(jsonKeyValueMap);
+			LOG.info(json);
+			System.out.println(json);
+
+			String test = null;
+			// cause NullPointerException
+			test.equals("");
+
+		} catch (Exception e) {
+			exitCode = 1;
+			LOG.error("処理中に例外が発生しました。", e);
+
+		} finally {
+			LOG.info("ステータスコード:" + exitCode);
+			LOG.info("処理を終了します。");
+		}
+
+		System.exit(exitCode);
 	}
 }
